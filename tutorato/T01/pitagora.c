df18af4aaa5fdfbf0faaaa38c6e914e66cf2f45e
sharon.cafa01 <sharon.cafa01@universitadipavia.it>
2020-01-15 09:10:37 +0000

    pit

---
+++ tutorato/T01/pitagora.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+
+int main ()
+
+{
+    double C1 = 3;
+    double C2 = 4;
+    double IPO;
+
+    IPO = sqrt(C1*C1 + C2*C2);
+
+    printf("Il valore dell'ipotenusa è: %.2f\n", IPO);
+}
