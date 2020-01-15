ba5db91eb104456ebcb3b9a9ff8bbb771a747e76
sharon.cafa01 <sharon.cafa01@universitadipavia.it>
2020-01-15 08:48:20 +0000

    somma

---
+++ tutorato/T01/somma.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+
+{
+    double a = 10;
+    double b = 15;
+    double somma;
+
+    somma = (a + b);
+
+    printf("Il valore del primo addendo è: %.2f\n", a);
+    printf("Il valore del secondo addendo è: %.2f\n", b);
+    printf("Il risultato è: %.2f\n", somma);
+
+return 0;
+}
