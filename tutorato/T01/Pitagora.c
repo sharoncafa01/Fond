6a38d1ff827f430fef0757be8ec443b318119e04
sharon.cafa01 <sharon.cafa01@universitadipavia.it>
2020-01-15 08:58:31 +0000

    pitagora

---
+++ tutorato/T01/Pitagora.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main()
+{
+    double C1, C2, IPO;
+
+    C1 = 3;
+    C2 = 4;
+    IPO = sqrt [ (C1 * C1 ) + (C2 * C2)]
+
+    printf("Il primo cateto vale: %.2f\n", C1);
+    printf("Il secondo cateto vale: %.2f\n", C2);
+    printf("L'ipotenusa misura: %.2f\n", IPO);
+
+return 0;
+}
