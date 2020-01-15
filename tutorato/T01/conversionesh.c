78858c1d9ba1f13477176f8e9df9e64b07d53595
sharon.cafa01 <sharon.cafa01@universitadipavia.it>
2020-01-15 08:11:08 +0000

    conversione

---
+++ tutorato/T01/conversionesh.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    double eu = 100;
+    double dollaro;
+    double tasso = 1.18213;
+
+    dollaro = (eu * tasso);
+
+    printf("Importo in euro: %.2f\n", eu);
+    printf("Importo in dollari: %.2f\n", dollaro);
+
+}
