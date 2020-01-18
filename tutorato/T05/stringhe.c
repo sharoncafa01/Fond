16bab555071083af680ada5e5df2499cfb2a1ffb
sharon.cafa01 <sharon.cafa01@universitadipavia.it>
2020-01-18 16:47:40 +0000

    l

--- tutorato/T05/stringhe.c
+++ tutorato/T05/stringhe.c
@@ -2,19 +2,24 @@
 #include <string.h>
 
 
-int trova_ultimo_separatore(const char *percorso) 
+int trova_ultimo_separatore(const char *percorso)
 {
     int pos = strlen(percorso) - 1;    /* tutta la lunghezza sulla linea di comando*/
     while (pos >= 0 && percorso[pos] != '/')
         pos--;
     return pos;   /* mi da come valore 0 o la posizione, mi restituisce la posizione a cui sono arrivato alla fine del controllo*/
-}                                                    /*funzione la quale, ricevuto un percorso come argomento, restituisce l'indice dell'ultimo 
-                                                    ultimo separatore (il carattere /) presente nella stringa, oppure un valore negativo se nessun 
+}                                                    /*funzione la quale, ricevuto un percorso come argomento, restituisce l'indice dell'ultimo
+                                                    ultimo separatore (il carattere /) presente nella stringa, oppure un valore negativo se nessun
                                                     separatore è presente. */
-                                                   
+
     void basename(const char *percorso, char *b) {
     int pos = trova_ultimo_separatore(percorso);   /*dichiaro il valore della posizione come intero richiamando la funzione per poi copiare in b*/
-    strcpy(b, percorso + pos + 1);   /*ho ultimo separatore piu tutta la stringa*/
+    int i = (strlen - 1);
+    int p = (pos + 1);
+    int v;
+
+    for(v = p; v > i; v++)
+    strcpy(b, percorso + 1);   /*ho ultimo separatore piu tutta la stringa*/
 }
 
 
@@ -22,21 +27,21 @@ void basename(const char *percorso, char *b);
 
 int main(int argc, char *argv[])    /*Per poter leggere il percorso da linea di comando*/
 {
-    
+
     char b[1024];
     char percorso[1024];
-    
+
     if (argc != 2) {
         printf ("Numero di parametri errato.\n");
         return -1;
     }
 
     strcpy(percorso, argv[1]);  /*copio dentro percorso cio che era sulla linea di comando ottenuto da argv1 */
-    
+
     basename(percorso,b);    /*richiamo della funzione*/
-    
+
     printf("BASENAME\n");
     puts(b);      /*stampa della stringa*/
-        
+
 return 0;
 }
---
+++ tutorato/T05/stringhesh.c
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int trova_ultimo_separatore(const char *percorso)
+{
+    int pos;
+    pos = (strlen(percorso)-1);
+
+    while (pos >= 0 && percorso[pos] != '/')
+          pos--;
+    return pos;
+}
+
+void basename(const char *percorso, char *b)
+{
+    int pos = (trova_ultimo_separatore(percorso) + 1);
+    int i;
+    int f = (strlen(percorso) - 1);
+
+    for(i = pos ; i < f; i++)
+          b[i] = percorso[i];
+
+    printf("%s\n", b);
+
+}
+
+void dirname(const char *percorso, char *b)
+{
+    int i;
+    int n = trova_ultimo_separatore(percorso);
+
+    if (n > 0 ) {
+       for(i = 0; i < n; i++)
+          b[i] = percorso[i];
+          b[n] = '\0';
+          puts(b);
+       }
+    else
+        b[0] = '\0';
+
+
+}
+
+int tipo_percorso(const char *percorso)
+{
+    if (percorso[0] == '\'') {
+        printf("ASSOLUTO\n");
+    }
+    else
+        printf("RELATIVO\n");
+}
+
+int trova_ultimo_punto(const char *percorso)
+{
+    int posi = (strlen(percorso) - 1);
+    while (posi >= 0 && percorso[posi] != '.')
+         posi--;
+
+    return posi;
+}
+
+int trova_ultimo_carattere(const char *percorso)
+{
+    int pnt = (trova_ultimo_punto(percorso) + 1);
+
+    if (percorso[pnt] != '\0')
+      printf("%s\n", percorso[pnt]);
+    else
+      printf("nessuna\n");
+}
+
+int main(int argc, char *argv[])
+{
+    int percorso[1024];
+    int pos[1024];
+    int b[1024];
+
+    if (argc != 2) {
+        printf ("Numero di parametri errato.\n");
+        return -1;
+    }
+
+    strcpy(percorso, *argv);
+
+    printf("[BASENAME]\n");
+    basename(percorso, b);
+
+    printf("[DIRNAME]\n");
+    dirname(percorso, b);
+
+    printf("[TIPO]\n");
+    tipo_percorso(percorso);
+
+    printf("[ESTENSIONE]\n");
+    trova_ultimo_carattere(percorso);
+
+return 0;
+}
